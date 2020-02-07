28ac0f667edb4998aa9de4e4c96d7feeca91c4e3
DavidFa <fajia19830811@gmail.com>
2020-02-07 22:38:37 +1100

    authentication

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+
 Route::get('/user', function (Request $request) {
     return Auth::user();
 });
